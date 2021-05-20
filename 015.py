34d64b9f3e0834180be701c5e5742d08871d7760
qiilee <qiilee@126.com>
2021-05-20 10:50:50 +0800

    fix: examples

--- 015.py
+++ 015.py
@@ -1,2 +1,38 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+# 题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
+
+# 程序分析：程序分析：(a>b) ? a:b 这是条件运算符的基本例子。
+
+# 程序源代码：
+
+
+# 实例(Python 2.x)
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+score = int(raw_input('输入分数:\n'))
+if score >= 90:
+    grade = 'A'
+elif score >= 60:
+    grade = 'B'
+else:
+    grade = 'C'
+ 
+print '%d 属于 %s' % (score,grade)
+
+
+
+# 实例(Python 3.x)
+#!/usr/bin/python3
+ 
+score = int(input('输入分数:\n'))
+if score >= 90:
+    grade = 'A'
+elif score >= 60:
+    grade = 'B'
+else:
+    grade = 'C'
+ 
+print ('%d 属于 %s' % (score,grade))
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Python 100例
 
-[代码来源](https://www.runoob.com/python/python-100-examples.html)
+Python 是一种解释型、面向对象、动态数据类型的高级程序设计语言。
+
+Python 由 Guido van Rossum 于 1989 年底发明，第一个公开发行版发行于 1991 年。
+
+像 Perl 语言一样, Python 源代码同样遵循 GPL(GNU General Public License) 协议。
+
+官方宣布，2020 年 1 月 1 日， 停止 Python 2 的更新。
+
+Python 2.7 被确定为最后一个 Python 2.x 版本。本项目的100个实例在Python2.7下测试通过，如有运行问题请参考[实例来源](https://www.runoob.com/python/python-100-examples.html)
+
+* [Python 2.x 版本的教程](https://www.runoob.com/python/python-tutorial.html)
+* [Python 3.X 版本的教程](https://www.runoob.com/python3/python3-tutorial.html)
