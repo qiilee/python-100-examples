414cc66bb94ed0a035f4a47a15f925a281cf2b49
qiilee <qiilee@126.com>
2021-05-20 10:28:48 +0800

    fix: examples

--- 008.py
+++ 008.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
-#coding:utf-8
+# -*- coding: UTF-8 -*-
+
+# 题目：输出 9*9 乘法口诀表。
+
+# 程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
+
+# 程序源代码：
+
+# 9*9 乘法口诀(Python 2.x)
+for i in range(1, 10):
+    print 
+    for j in range(1, i+1):
+        print "%d*%d=%d" % (i, j, i*j),
+
+
+
+# 9*9 乘法口诀(Python 3.x)
+#!/usr/bin/python3
+ 
+for i in range(1, 10):
+    print() 
+    for j in range(1, i+1):
+        print ("%d*%d=%d" % (i, j, i*j), end=" " )
--- 010.py
+++ 010.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
-#coding:utf-8
+# -*- coding: UTF-8 -*-
+
+# 题目：暂停一秒输出，并格式化当前时间。
+
+# 程序分析：无。
+
+# 程序源代码：
+
+import time
+ 
+print (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+ 
+# 暂停一秒
+time.sleep(1)
+ 
+print (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
